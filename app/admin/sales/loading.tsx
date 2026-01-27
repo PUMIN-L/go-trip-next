892d3d61099bb75847939a1660c4641bf8ac8716
PUMIN-L <puminbg@gmail.com>
2026-01-27 17:41:28 +0700

    add loading

---
+++ app/admin/sales/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingTable from "@/global/LoadingTable"
+
+function loading() {
+  return <LoadingTable />
+}
+
+export default loading
--- global/LoadingTable.tsx
+++ global/LoadingTable.tsx
@@ -1,13 +1,18 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
-function LoadingTable({ rows = 5 }: { rows?: number }) {
+function LoadingTable({ rows = 6 }: { rows?: number }) {
   const tableRows = Array.from({ length: rows }, (_, index) => {
     return (
-      <div className="mb-4" key={index}>
+      <div className="mb-6" key={index}>
         <Skeleton className="w-full h-8 rounded" />
       </div>
     )
   })
-  return <>{tableRows}</>
+  return (
+    <>
+      {tableRows}
+      <Skeleton className="w-50 h-8 rounded m-auto" />
+    </>
+  )
 }
 export default LoadingTable
